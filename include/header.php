641116ff64629bde59114fd72315f9293bd381de
unknown <bugattitarikou93@gmail.com>
2018-12-11 18:16:59 +0100

    evenement with notif

--- include/header.php
+++ include/header.php
@@ -1,8 +1,14 @@
+<?php
+if (!isset($_SESSION))
+  {
+    session_start();
+  }
+ ?>
 	<div class="pre-loader"></div>
 	<div class="header clearfix">
 		<div class="header-right">
 			<div class="brand-logo">
-				<a href="index.php">
+				<a href="#">
 					<img src="vendors/images/logo.png" alt="" class="mobile-logo">
 				</a>
 			</div>
@@ -16,73 +22,59 @@
 				<div class="dropdown">
 					<a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown">
 						<span class="user-icon"><i class="fa fa-user-o"></i></span>
-						<span class="user-name">Johnny Brown</span>
+						<span class="user-name"><?php echo $_SESSION['user'] ?></span>
 					</a>
 					<div class="dropdown-menu dropdown-menu-right">
-						<a class="dropdown-item" href="profile.php"><i class="fa fa-user-md" aria-hidden="true"></i> Profile</a>
-						<a class="dropdown-item" href="profile.php"><i class="fa fa-cog" aria-hidden="true"></i> Setting</a>
-						<a class="dropdown-item" href="faq.php"><i class="fa fa-question" aria-hidden="true"></i> Help</a>
-						<a class="dropdown-item" href="login.php"><i class="fa fa-sign-out" aria-hidden="true"></i> Log Out</a>
+						<a class="dropdown-item" href="#"><i class="fa fa-user-md" aria-hidden="true"></i> Profile</a>
+						<a class="dropdown-item" href="#"><i class="fa fa-question" aria-hidden="true"></i> Help</a>
+						<a class="dropdown-item" href="#"><i class="fa fa-cog" aria-hidden="true"></i> Setting</a>
+            <a class="dropdown-item" href="#" onclick="deconn()"><i class="fa fa-sign-out" aria-hidden="true"></i> Log Out</a>
+
+						<form id="deconnexionForm"  action="servlets/deconnexion.php" method="post">
+
+						</form>
+						<script type="text/javascript">
+							function deconn(){
+								document.getElementById("deconnexionForm").submit();
+							}
+						</script>
 					</div>
 				</div>
 			</div>
 			<div class="user-notification">
 				<div class="dropdown">
 					<a class="dropdown-toggle no-arrow" href="#" role="button" data-toggle="dropdown">
-						<i class="fa fa-bell" aria-hidden="true"></i>
-						<span class="badge notification-active"></span>
+            <i class="fa fa-bell" aria-hidden="true"></i>
+
+            <?php
+                if($_SESSION['notif'] > 0){
+                  echo '<span >'. $_SESSION['notif']  .'</span>';
+                }
+
+
+             ?>
+
 					</a>
 					<div class="dropdown-menu dropdown-menu-right">
 						<div class="notification-list mx-h-350 customscroll">
 							<ul>
 								<li>
-									<a href="#">
-										<img src="vendors/images/img.jpg" alt="">
-										<h3 class="clearfix">John Doe <span>3 mins ago</span></h3>
-										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed...</p>
-									</a>
-								</li>
-								<li>
-									<a href="#">
-										<img src="vendors/images/img.jpg" alt="">
-										<h3 class="clearfix">John Doe <span>3 mins ago</span></h3>
-										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed...</p>
-									</a>
-								</li>
-								<li>
-									<a href="#">
-										<img src="vendors/images/img.jpg" alt="">
-										<h3 class="clearfix">John Doe <span>3 mins ago</span></h3>
-										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed...</p>
-									</a>
-								</li>
-								<li>
-									<a href="#">
-										<img src="vendors/images/img.jpg" alt="">
-										<h3 class="clearfix">John Doe <span>3 mins ago</span></h3>
-										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed...</p>
-									</a>
-								</li>
-								<li>
-									<a href="#">
-										<img src="vendors/images/img.jpg" alt="">
-										<h3 class="clearfix">John Doe <span>3 mins ago</span></h3>
-										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed...</p>
-									</a>
-								</li>
-								<li>
-									<a href="#">
-										<img src="vendors/images/img.jpg" alt="">
-										<h3 class="clearfix">John Doe <span>3 mins ago</span></h3>
-										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed...</p>
-									</a>
-								</li>
-								<li>
-									<a href="#">
-										<img src="vendors/images/img.jpg" alt="">
-										<h3 class="clearfix">John Doe <span>3 mins ago</span></h3>
-										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed...</p>
-									</a>
+                  <?php
+                      if($_SESSION['notif'] > 0){
+                      echo '<a href="listEvent.php" >
+    										   <img src="vendors/images/img.jpg" alt="">
+    										   <h4 class="clearfix"> vous avez '. $_SESSION['notif']  .' évènement(s) demain </h4>
+    									   </a>';
+                      }
+
+                      else{
+                        echo '<a href="#">
+      										<img src="vendors/images/img.jpg" alt="">
+      										<h4 class="clearfix"> vous n\'avez aucun évènement demain </h4>
+      									</a>';
+                      }
+
+                   ?>
 								</li>
 							</ul>
 						</div>
@@ -91,3 +83,9 @@
 			</div>
 		</div>
 	</div>
+
+
+
+  <form id="formNotif" action="servlets/addEvent1.php" method="post">
+
+  </form>
